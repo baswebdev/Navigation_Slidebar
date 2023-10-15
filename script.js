c2e98a56e5c3236ffb5969c64d0d671aef5d2f57
Burak Akbas <127743530+baswebdev@users.noreply.github.com>
2023-10-15 21:08:46 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSlidebar() {
+    document.getElementById("mySlidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSlidebar() {
+    document.getElementById("mySlidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
